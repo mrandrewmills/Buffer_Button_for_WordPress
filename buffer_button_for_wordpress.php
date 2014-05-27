90fbd50bd2cd86f94de5a3dc2578d670be525967
mrandrewmills <andrewmills@knowwwhow.com>
2014-05-26 20:17:38 -0400

    start i18n prep
    Add languages folder. Add text domain and domain path to plugin header.

--- buffer_button_for_wordpress.php
+++ buffer_button_for_wordpress.php
@@ -1,15 +1,17 @@
 <?php
 /**
  * @package buffer_button_for_WordPress
- * @version 0.01
+ * @version 0.02
  */
 /*
 Plugin Name: Buffer Button for WordPress
 Plugin URI: http://mrandrewmills.com/buffer-button-for-wordpress
 Description: create and manage buttons that empower readers to share your content with their Buffer accounts.
 Author: Andrew Mills
-Version: 0.01
+Version: 0.02
 Author URI: http://mrandrewmills.com/
+Text Domain: buffer-button-for-wordpress
+Domain Path: /languages
 */
 
 /* The action hook and corresponding function to create a submenu item under Media Library */
@@ -27,13 +29,24 @@ function show_buffer_button_options() {
 	}
 	echo '<div class="wrap">';
 	echo '<div id="icon-upload" class="icon32"><br></div>';
-	echo '<h2>Buffer Buttons</h2>';
+	echo '<h2>Buffer Buttons for WordPress</h2>';
+	echo '<p>Style:</p>';
+	echo '<p>Tweet text:</p>';
+	echo '<p>URL:</p>';
+	echo '<p>Twitter Username to mention:</p>';
+	echo '<p>Picture URL:</p>';
 
 	/* end of the wrap div */
 	echo '</div>';
 }
 
 function init() {
+
+}
+
+function buffer_button_output() {
+
+	// to do: dynamically generated output, based on settings for a given form.
 
 }
 
